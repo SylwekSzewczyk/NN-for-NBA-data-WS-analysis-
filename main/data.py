2de23dcc9361024a71caef23a95bc7d872b02871
SylwekSzewczyk <sylwekszewczyk@interia.pl>
2019-07-19 23:52:06 +0200

    'update'

--- main/data.py
+++ main/data.py
@@ -6,10 +6,11 @@ Created on Fri Jul 12 22:07:14 2019
 """
 
 import pandas as pd
-import seaborn as ss
+import seaborn as sns
 
 #1 Data cleaning
 
+#a) 2018/2019 data
 df = pd.read_html('https://www.basketball-reference.com/leagues/NBA_2019_advanced.html')[0]
 df.drop_duplicates(subset = 'Player', keep='first', inplace=True)
 df = df[df['Player'] != 'Player']
@@ -21,8 +22,23 @@ df.insert(22, 'WShares', df['WS'], allow_duplicates = True)
 df.insert(23, 'WShares/48', df['WS/48'], allow_duplicates = True)
 df.drop(['WS', 'WS/48'], axis = 1, inplace = True)
 
+#b) 2004/2005 data
+
+df1 = pd.read_html('https://www.basketball-reference.com/leagues/NBA_2005_advanced.html')[0]
+df1.drop_duplicates(subset = 'Player', keep='first', inplace=True)
+df1 = df1[df1['Player'] != 'Player']
+df1.drop(['Unnamed: 19', 'Unnamed: 24', 'Pos', 'Age', 'Tm','Rk','Player'], axis = 1, inplace = True)
+df1[df1.columns.values.tolist()] = df1[df1.columns.values.tolist()].astype(float)
+df1[['G', 'MP']] = df1[['G', 'MP']].astype(int)
+df1 = df1[df1['G'] >= 10]
+df1.insert(22, 'WShares', df1['WS'], allow_duplicates = True)
+df1.insert(23, 'WShares/48', df1['WS/48'], allow_duplicates = True)
+df1.drop(['WS', 'WS/48'], axis = 1, inplace = True)
+
 #2 Data checking
 
+sns.heatmap(df.corr(), annot=True)
+sns.distplot(df['WShares/48'])
 
-
-
+sns.heatmap(df1.corr(), annot=True)
+sns.distplot(df1['WShares/48'])
--- main/nn.py
+++ main/nn.py
@@ -8,7 +8,10 @@ Created on Thu Jul 18 22:10:26 2019
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from data import df
+from data import df, df1
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
 
 #1 Data preprocessing
 
@@ -20,3 +23,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
+
+#2 NN architecture
+
+nn = Sequential()
+
